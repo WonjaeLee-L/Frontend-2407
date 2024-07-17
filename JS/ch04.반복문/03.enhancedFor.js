c2a12a9bcb1ff1639f0a7a060dacb8fdd2884e56
Wonjae Lee <dldnjswo@gmail.com>
2024-07-17 11:38:02 +0900

    Javascript

--- JS/ch04.반복문/03.enhancedFor.js
+++ JS/ch04.반복문/03.enhancedFor.js
@@ -7,12 +7,28 @@ let summerFruits = ['수박', '참외', '복숭아', '포도'];
 
 // for (let i in summerFruits) {
 //     console.log(i, summerFruits[i]);
-// }
+// }                                            object에서 in 사용
 
-// 배열의 모든 요소에 대해 출력을 하라!는 의미
+// 배열의 모든 요소에 대해 출력을 하라!는 의미  array에서 of 사용
 for (let fruit of summerFruits) {
     console.log(fruit);
 }
 
 // 여름 과일들을 하나의 문자열로 만들기('') 더하기 할때에는 0이었음
-let favoriteFruits = '';
+// if문과 마찬가지로 for아래에 한문장이면 {}를 열 필요 없음
+let favoriteFruits = '';    // 문자열일 경우 ''
+for (let fruit of summerFruits)
+    favoriteFruits += fruit + ' ';
+console.log(favoriteFruits);
+
+// 1에서 10까지 곱한 값(10factorial)
+let product = 1;        // 곱일 경우에 1
+for (let i = 1; i <= 10; i++)
+    product *= i;
+console.log(product);
+
+// 1에서 30까지 3의 배수의 배열 만들기 [3, 6, 9, ..., 30]
+let multiple3 = [] ;    // 배열일 경우 []
+for (let i = 3; i <= 30; i += 3)
+    multiple3.push(i);
+console.log(multiple3);
---
+++ JS/ch04.반복문/04.while.js
@@ -0,0 +1,21 @@
+// While 반복문
+// 형태는 while (불 표현식) {}
+// 불표현식을 만족하는 동안 문장 반복.
+// 조건을 변화하는 실행문이 반드시 필요함. 없으면 무한 루프
+let summerFruits = ['수박', '참외', '복숭아', '포도'];
+
+let index = 0;
+while (index < summerFruits.length) {
+    console.log(summerFruits[index]);
+    index++;
+}
+
+// 주사위를 던진 합이 21을 초과할 때 loop 탈출하고
+// 그간 던진 주사위 눈을 출력하기
+let sumDice = 0;
+while (sumDice <= 21) {
+    let dice = Math.ceil(Math.random() * 6);
+    console.log(dice);
+    sumDice += dice;
+}
+console.log(sumDice);
