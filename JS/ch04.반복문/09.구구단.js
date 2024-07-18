098f62aadaaec8c9589e7e81c981341ff4df6f77
Wonjae Lee <dldnjswo@gmail.com>
2024-07-18 09:13:16 +0900

    JS 반복문

--- JS/ch04.반복문/09.구구단.js
+++ JS/ch04.반복문/09.구구단.js
@@ -10,13 +10,17 @@ for (let i = 2; i <=9; i++) {
 let N = 4;      // 가운데 두꺼운 별이 9개
 for (let i = 0; i <= N; i++) {
     let line = '';
-    for (let k = 0; k < N - i; k++)
+    for (let k = 0; k < N - i; k++) 
         line += ' ';
     for (let k = 0; k < 2 * i + 1; k++)
         line += '*';
     console.log(line);
 }
 for (let i = N - 1; i >= 0; i--) {
-    
-
+    let line = '';
+    for (let k = 0; k < N - i; k++) 
+        line += ' ';
+    for (let k = 0; k < 2 * i + 1; k++)
+        line += '*';
+    console.log(line);
 }
